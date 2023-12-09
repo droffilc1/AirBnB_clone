82ed69b5aa6a4f807288611e590f890fe64c4851
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-12-09 16:45:53 +0300

    fix: typo Console 1.0

--- console.py
+++ console.py
@@ -7,6 +7,12 @@ from datetime import datetime
 import shlex
 from models import storage
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     """Entry point of the command interpreter"""
@@ -86,7 +92,7 @@ class HBNBCommand(cmd.Cmd):
         command = self.parseline(line)[0]
         objs = storage.all()
         if command is None:
-            print([str(objs[objs]) for obj in objs])
+            print([str(objs[obj]) for obj in objs])
         elif command in self.classes:
             keys = objs.keys()
             print([str(objs[key]) for key in keys if key.startswith(command)])
