aa6a35767406468287def0020b04a2e433aba20f
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-12-09 11:59:06 +0300

    feat: More classes

---
+++ models/city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""city module"""
+
+
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Inherits from BaseModel
+
+    Attributes:
+        state_id: string - empty string: it will be the State.id.
+        name: string - empty string.
+    """
+    state_id = ''
+    name = ''
---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" state module """
+
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Inherits from BaseModel
+
+    Attributes:
+        name: string - empty string.
+    """
+    name = ''
