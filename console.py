137fbf2e7cc221de640cca16440d242260a59258
Clifford Mapesa <cliffordmapesa8@gmail.com>
2023-12-10 09:10:08 +0300

    fix: requirement

--- console.py
+++ console.py
@@ -18,7 +18,8 @@ from models.review import Review
 class HBNBCommand(cmd.Cmd):
     """Entry point of the command interpreter"""
     prompt = '(hbnb) '
-    classes = ['BaseModel']
+    classes = ['BaseModel', 'User', 'State', 'City', 'Amenity',
+               'Place', 'Review']
 
     def do_EOF(self, line):
         """Handle EOF"""
@@ -39,29 +40,59 @@ class HBNBCommand(cmd.Cmd):
             return
 
         try:
-            # Dynamically instantiate the class based on
-            # the provided class name
             class_name = line.split()[0]
-            new_instance = globals()[class_name]()
+            if class_name not in self.classes:
+                print("** class doesn't exist **")
+                return
+            model_class = globals()[class_name]
+            new_instance = model_class()
             new_instance.save()
             print(new_instance.id)
         except KeyError:
             print("** class doesn't exist **")
 
+    def parse_line_args(self, line):
+        """Parses line arguments and returns class name and instance id"""
+        command, arg = self.parseline(line)
+        if command == '':
+            print("** class name missing **")
+            return None, None
+        if command not in self.classes:
+            print("** class doesn't exist **")
+            return None, None
+        if arg == '':
+            print("** instance id missing **")
+            return command, None
+        return command, arg
+
+    def common_checks(self, command, arg):
+        """Common checks for show, destroy, and update methods"""
+        if command is None or arg is None:
+            return False
+        key = command + '.' + arg
+        data = storage.all().get(key)
+        if data is None:
+            print("** no instance found **")
+            return False
+        return True
+
     def do_show(self, line):
         """Prints the string representation of an instance
         based on the class name and id"""
-        command = self.parseline(line)[0]
-        arg = self.parseline(line)[1]
+        args = line.split()
 
-        if command is None:
+        if not args:
             print("** class name missing **")
-        elif command not in self.classes:
-            print("** class doesn't exist **")
-        elif arg == '':
+        elif len(args) < 2:
             print("** instance id missing **")
         else:
-            data = storage.all().get(command + '.' + arg)
+            command = args[0]
+            arg = args[1]
+            if command not in self.classes:
+                print("** class doesn't exist **")
+            else:
+                key = command + '.' + arg
+                data = storage.all().get(key)
             if data is None:
                 print("** no instance found **")
             else:
@@ -69,34 +100,42 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, line):
         """Deletes an instance based on the class name and id"""
-        command = self.parseline(line)[0]
-        arg = self.parseline(line)[1]
+        args = shlex.split(line)
+        args_size = len(args)
 
-        if command is None:
+        if args_size == 0:
             print("** class name missing **")
-        elif command not in self.classes:
+            return
+        if args[0] not in self.classes:
             print("** class doesn't exist **")
-        elif arg == '':
+            return
+        if args_size == 1:
             print("** instance id missing **")
-        else:
-            key = command + '.' + arg
-            data = storage.all().get(key)
-            if data is None:
-                print("** no instance found **")
-            else:
-                del storage.all()[key]
-                storage.save()
+            return
+
+        command, instance_id = args
+
+        key = command + '.' + instance_id
+        data = storage.all().get(key)
+
+        if data is None:
+            print("** no instance found **")
+            return
+
+        del storage.all()[key]
+        storage.save()
 
     def do_all(self, line):
         """Prints all string representation of all
         instances based or not on the class name"""
         command = self.parseline(line)[0]
         objs = storage.all()
-        if command is None:
-            print([str(objs[obj]) for obj in objs])
-        elif command in self.classes:
+        if command is None or command in self.classes:
             keys = objs.keys()
-            print([str(objs[key]) for key in keys if key.startswith(command)])
+            print(
+                [str(objs[key]) for key in keys
+                    if command is None or key.split('.')[0] == command]
+            )
         else:
             print("** class doesn't exist **")
 
@@ -105,26 +144,36 @@ class HBNBCommand(cmd.Cmd):
         and id by adding or updating attribute"""
         args = shlex.split(line)
         args_size = len(args)
+
         if args_size == 0:
             print("** class name missing **")
-        elif args[0] not in self.classes:
+            return
+        if args[0] not in self.classes:
             print("** class doesn't exist **")
-        elif args_size == 1:
+            return
+        if args_size == 1:
             print("** instance id missing **")
-        else:
-            key = args[0] + '.' + args[1]
-            instance_data = storage.all().get(key)
-            if instance_data is None:
-                print("** no instance found **")
-            elif args_size == 2:
-                print("** attribute name missing **")
-            elif args_size == 3:
-                print("** value missing **")
-            else:
-                args[3] = self.analyze_parameter_value(args[3])
-                setattr(instance_data, args[2], args[3])
-                setattr(instance_data, 'updated_at', datetime.now())
-                storage.save()
+            return
+
+        command, instance_id, *attributes = args
+
+        key = command + '.' + instance_id
+        instance_data = storage.all().get(key)
+
+        if instance_data is None:
+            print("** no instance found **")
+            return
+
+        if len(attributes) < 2:
+            print("** attribute name or value missing **")
+            return
+
+        attribute_name, value = attributes[0], attributes[1]
+        value = self.analyze_parameter_value(value)
+
+        setattr(instance_data, attribute_name, value)
+        setattr(instance_data, 'updated_at', datetime.now())
+        storage.save()
 
     def analyze_parameter_value(self, value):
         """Checks a parameter value for an update
